ed0c13c9fe4843d89fe0f70c4044bc7491daa61e
antonmylinchuk <150524995+antonmylinchuk@users.noreply.github.com>
2024-11-18 18:24:29 +0200

    Update SQL_PROJECT_LEETCODE_SOLUTION.sql

--- SQL/SQL_PROJECT_LEETCODE_SOLUTION.sql
+++ SQL/SQL_PROJECT_LEETCODE_SOLUTION.sql
@@ -1,4 +1,4 @@
--- This file provides my solution and detailed explanation
+-- This file provides my solution and a detailed explanation
 -- to the Leetcode task called '185. Department Top Three Salaries'
 -- Task Link: https://leetcode.com/problems/department-top-three-salaries/description/
 
@@ -65,49 +65,49 @@
 
 -- Let's solve the problem step by step:
 
--- 1. First, I need to find a name of the department for each employee. 
+-- 1. First, I need to find the name of the department for each employee. 
 -- To do this, I will join the table 'Department' to the 'Employee' table.
--- I am going to use 'INNER JOIN' for joining the tables.
+-- I will use 'INNER JOIN' to join the tables.
 SELECT *
   FROM employee AS e
        INNER JOIN departmentid AS d
        ON e.departmentid = d.id
 
--- Let's display only department name, employee name and a salary columns:
+-- Let's display only department name, employee name, and salary columns:
 SELECT d.name, e.name, e.salary
   FROM employee AS e
        INNER JOIN departmentid AS d
        ON e.departmentid = d.id
 
--- Now we have employees, the departments they work in and employee salaries.
+-- Now we have employees, the departments they work in, and employee salaries.
 -- In other words, we have departments, the employees that work in corresponding departments, 
--- and all of the salaries of employees that belongs to corresponding departments. 
+-- and all of the salaries of employees that belong to corresponding departments. 
 -- Thus, we have all of the salaries of the departments.
 
 -- 2. Second, I need to find the top three unique salaries for each of the departments that have salaries.
 -- To do this, I need to rank salaries in a way where the highest salary of the department would get the highest rank '1'.
 -- The second highest would get '2' rank and the third would get '3'. If the salaries are the same, they should have the same rank.
--- To do this, I am going to use the 'DENSE_RANK' window function. 
--- The function assigns consecutive ranks without gaps, withing its partition. 
+-- To do this, I will use the 'DENSE_RANK' window function. 
+-- The function assigns consecutive ranks without gaps, within its partition. 
 -- If values are the same, they receive the same rank. 
--- The fuctnion should be used with 'ORDER BY' clause to rank the values in the desired order 
+-- The function should be used with the 'ORDER BY' clause to rank the values in the desired order 
 -- (highest values receive the highest ranks or the lowest values receive the highest ranks)
--- I am going to partition the table by the 'd.name' column, becuase I need rank salaries for each of the departments.
--- Also, I need to sort the salary values in the descending order, because I want the highest salaries receive the highest rank.
+-- I will partition the table by the 'd.name' column because I need to rank salaries for each of the departments.
+-- Also, I need to sort the salary values in descending order, because I want the highest salaries to receive the highest rank.
 SELECT d.name, e.name, e.salary,
        DENSE_RANK() OVER(PARTITION BY d.id ORDER BY e.salary DESC)
   FROM employee AS e
        INNER JOIN departmentid AS d
        ON e.departmentid = d.id
 
--- 3. Next, I want to display only top three salaries for each of the departments, 
+-- 3. Next, I want to display only the top three salaries for each of the departments, 
 -- the employees who have such salaries and the departments they work in.
 -- I need to use the 'WHERE' clause to filter the records. But If I apply the condition in this query it will not work,
 -- because window functions are evaluated after the 'WHERE' clause.
--- Becuase of the fact that the 'WHERE' clause is executed firstly, 
--- 'WHERE' clause can not reference a calculation derived from a window function because it does not exist yet.
--- To overcome this, I need either use the query as a subquery or use the query as a CTE (Common Table Expression)
--- I prefer to use a CTEs over the subqueries because it allows not only structurize queries into logical order, 
+-- Because of the fact that the 'WHERE' clause is executed first, 
+-- The 'WHERE' clause can not reference a calculation derived from a window function because it does not exist yet.
+-- To overcome this, I need to either use the query as a subquery or use the query as a CTE (Common Table Expression)
+-- I prefer to use CTEs over the subqueries because it allows not only structuring queries into logical order, 
 -- but also it is easier to read and maintain:
   WITH cte AS
 (
@@ -119,7 +119,7 @@ SELECT d.name, e.name, e.salary,
 )
 
 -- To use custom names for the columns 
--- I can assing them within parentheses after the name of the CTE:
+-- I can assign them within parentheses after the name of the CTE:
   WITH cte (department, employee, salary, salary_rank) AS
 (
 SELECT d.name, e.name, e.salary,
